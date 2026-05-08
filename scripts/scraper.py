9a3fefd686b74705b259bdbbe219928f0128454f
debhere <debmalya.mondal@outlook.com>
2026-05-08 09:28:28 +0530

    doctstring updated

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -44,6 +44,26 @@ def getSoup(url: str) -> BeautifulSoup:
 
 
 def getPagesForInsurancePlans(soup: BeautifulSoup) -> Dict:
+    """LIC has different categories for it's insurance products. There are multiple plans for each category. Each
+    product has a dedicated url as well. This function extracts the html page link for each of the insurance plans 
+    and maps it in a python dictionary.
+
+    At first, all the links ('a' in html) are extracted into 'elements' through a list comprehension where there is a 
+    sub list ('li.has-sub'). This now contains the web page url for all products.
+    
+    'elems' is the list where it filters out other html pages and only keeps the plans for the insurance
+    products.
+
+    Then in the end the title for each of the wb page is extracted to identify the insurance category and puts
+    inside a python dictionary along with it's web page url.
+
+    Args:
+        soup: BeautifulSoup, a BeautifulSoup object.
+
+    Returns:
+        Dict: policy_map, A dictionary containing the urls corresponding to each insurance category.
+
+    """
     elements = [elem.find('a').find_next('ul', class_='child-menu') for elem in soup.select('li.has-sub')]
     elems = set(elem.find_next('ul', class_="child-menu") for elem in elements if "Insurance" in elem.find_next('li').find_next('a')['title']\
          and "Plan" in elem.find_next('li').find_next('a')['title'])
@@ -59,6 +79,23 @@ def getPagesForInsurancePlans(soup: BeautifulSoup) -> Dict:
 
 
 def getInsurancePlanLinks(policyPage: Dict) -> Dict:
+    """For each category of insurance products, there are multiple policies and each policy has it's
+    own url. This function maps the insurance category to it's policy and the url which means that this
+    function takes a insurance-category vs web-page dictionary as the argument and returns a nested python 
+    dictionary that contains insurance-category, the corresponding policies and the urls. The form of the dictionary
+    will be like: 
+    {'insurance-category1': {'plan1': 'url1', 'plan2': 'url2'}, 'insurance-category2': {'plan1': 'url1', 'plan2': 'url2'}} 
+
+    Pass each webpage url through a soup object to parse the html elements. In a dictionary comprehension
+    extract the urls for each policy with certain filters in place and map it with the product caregory.
+
+    Args: 
+        policyPage: Dict, a dictionary containing the insurance-product and the their webpage url.
+
+    Returns:
+        Dict: policy_links, a python dictionary containing insurance-product and it's policies with the 
+        urls.
+    """
     policy_links = {}
     for p, l in policyPage.items():
         soup = getSoup(l)
