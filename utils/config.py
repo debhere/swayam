5ddb914cf23f6e536de7c4220aa013788e1fa3f9
debhere <debmalya.mondal@outlook.com>
2026-05-08 22:02:44 +0530

    configuration

---
+++ utils/config.py
@@ -0,0 +1,13 @@
+BASE_URL: str = "https://licindia.in"
+# URL: str = f"{BASE_URL}/web/guest/insurance-plan"
+
+BASE_FOLDER: str = "data"
+RAW_DATA_FOLDER: str = f"{BASE_FOLDER}/raw"
+
+RAW_DATASTORE: str = f"{BASE_FOLDER}/{RAW_DATA_FOLDER}"
+
+KNOWLEDGE_BASE: str = "knowledge-base"
+
+DB_NAME: str = "vector_db"
+
+LOGS: str = "logs"
