7f8dabd13488219dfa08f65c87ad9482a41fff18
debhere <debmalya.mondal@outlook.com>
2026-09-08 22:25:06 +0530

    project constants

---
+++ backend/app/config/constants.py
@@ -0,0 +1,18 @@
+from typing import List
+
+LIC_BASE_URL: str = "https://licindia.in/web/guest/"
+# COMMON_STRING: str = "/web/guest/"
+# PRODUCT_PAGE: str = "products"
+
+PRODUCT_CATEGORIES: List[str] = ['insurance-plan', 'pension-plan', 'unit-linked-plans', 
+                                 'micro-insurance-plans', 'withdrawn-plans']
+
+
+PARENT_DATA_DIR: str = "data"
+RAW_DATA_DIR: str = "raw"
+PROCESSED_DATA_DIR: str = "processed"
+METADATA_DIR: str = "metadata"
+
+BASE_LOG_DIR: str = "logs"
+
+
