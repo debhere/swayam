b24da59befda7cbaec829dbd95ed98f677e63225
debhere <debmalya.mondal@outlook.com>
2026-09-12 16:11:10 +0530

    ingestion module - cleaned, normalized, bug fixed

--- backend/app/ingestion/downloader.py
+++ backend/app/ingestion/downloader.py
@@ -1,12 +1,10 @@
 import sys
-import requests
 
-from bs4 import BeautifulSoup
-
-from pathlib import Path
-from typing import List
+from app.ingestion.insurance import download_insurance_docs
+from app.ingestion.other_products import download_other_product_docs
 
 from app.config import constants
+
 from app.utils.exception import CustomException
 from app.utils.logger import setup_logging
 import logging
@@ -15,95 +13,27 @@ setup_logging()
 logger = logging.getLogger(__name__)
 
 
-def get_contents(url: str) -> bytes:
-    return requests.get(url).content
-
-def get_html_contents(url: str) -> BeautifulSoup:
-    contents: bytes = requests.get(url).content
-    soup = BeautifulSoup(contents, 'html.parser')
-    return soup
-
-def save_documents(product: str, base_url: str, documents: List[str], 
-                   policy: str, category: str = None):
-    HOME_DIR: str = Path(__file__).parents[2]
-
-    RAW_DOC_LOC: str = f"{HOME_DIR}/{constants.PARENT_DATA_DIR}/{constants.RAW_DATA_DIR}"
-    Path(RAW_DOC_LOC).mkdir(parents=True, exist_ok=True)
-
-    if product.lower() == 'insurance':
-        INS_RAW_LOC: str = f"{RAW_DOC_LOC}/{product}"
-        Path(INS_RAW_LOC).mkdir(parents=True, exist_ok=True)
-
-        for document in documents:
-            doc_url: str = f"{base_url}{document}"
-
-            policy_raw_loc: str = f"{INS_RAW_LOC}/{category}"
-            Path(f"{policy_raw_loc}").mkdir(parents=True, exist_ok=True)
-
-            filepath = Path(f"{policy_raw_loc}/{policy}.pdf")
-
-            with open(filepath, 'wb') as f:
-                f.write(requests.get(doc_url).content)
-
-
-
-def download_insurance_docs(base_url: str, prd_str: str, lic_url: str):
-    try:
-        prd_url: str = f"{base_url}{prd_str}"
-        ins_content: BeautifulSoup = get_html_contents(prd_url)
-
-        elements: List[str] = ins_content.find_all('li', attrs={'class': 'selected'})
-        # all_anchors: List[str] = [lst.find_all('a') for lst in element]
-
-        category_urls, categories = [], []
-        
-        for element in elements:
-            for anchor in element.find_all('a'):
-                category_urls.append(anchor.get('href'))
-                categories.append(anchor.get_text().lower().replace(' ', '-'))
-            
-
-        #urls for each insurance category
-        # category_urls: List[str] = [l.get('href') for l in all_anchors]
-
-        # capturing insurance category names
-        # categories: List[str] = [l.get_text().lower().replace(' ', '-') for l in category_urls]
-
-        # navigating to each insurance category 
-        for idx, cat_url in enumerate(category_urls):
-            cat_content: BeautifulSoup = get_html_contents(cat_url)
-            policy_rows: List[str] = cat_content.find_all('td')
-
-            for row in policy_rows:
-                for policy_link in row.find_all('a'):
-                    policy_url: str = f"{lic_url}{policy_link.get('href')}"
-
-                    print(policy_url)
-
-                    policy_details: BeautifulSoup = get_html_contents(policy_url)
-
-                    anchors: List[str] = policy_details.find_all('section', {'id': 'maincontent'})[0].find_all('a')
-                    links: List[str] = [anchor.get('href') for anchor in anchors]
-                    policy: str = policy_details.find_all('section', {'id': 'maincontent'})[0].find('h5').get_text()
-                    save_documents("insurance", constants.LIC_WEB, 
-                                links, policy, categories[idx])
-
-    except Exception as e:
-        raise CustomException(e, sys)
-
-
-
-
 def main():
+    """Main controller of the ingestion module. Invokes the product functions for each LIC product
+    and downloads the corresponding artefacts.
+    """
     try:
         base_url: str = constants.LIC_BASE_URL
         lic: str = constants.LIC_WEB
 
         for product, var in constants.PRODUCT_CATEGORIES.items():
-            # url: str = f"{base_url}{var}"
+            logger.info(f"Invoking {product} module...")
             if product == 'insurance':
                 download_insurance_docs(base_url, var, lic)
 
+            elif product in ['pension', 'ulp', 'mip']:
+                download_other_product_docs(base_url, var, lic, product)
+
+            elif product == 'wp':
+                logger.info("Withdrawn products are not included yet...!!")
+            else:
+                logger.info("Unrecognized product...exiting!!")
+                sys.exit(1)
 
     except Exception as e:
         raise CustomException(e, sys)
--- backend/app/ingestion/insurance.py
+++ backend/app/ingestion/insurance.py
@@ -5,9 +5,8 @@ import logging
 from typing import List
 from bs4 import BeautifulSoup
 
-from app.ingestion.helper import save_documents
-from app.ingestion.helper import get_contents
-from app.ingestion.helper import get_html_contents
+from app.ingestion.common import save_documents, get_policy_urls, get_policy_details
+from app.ingestion.helper import get_html_contents, get_relevant_documents
 
 from app.utils.exception import CustomException
 from app.utils.logger import setup_logging
@@ -16,17 +15,26 @@ setup_logging()
 logger = logging.getLogger(__name__)
 
 def download_insurance_docs(base_url: str, prd_str: str, lic_url: str):
+    """Primary function to download the insurance policy artefacts for different LIC products. 
+
+     Args:
+        base_url: str, The base url of the product.
+        prd_str: str, The relevant product string in the LIC website
+        lic_url: str, LIC website.
+    """
     try:
-        logger.info("Navigating to LIC's insurance product module...")
+        logger.info("Navigating to LIC's insurance module...")
         
+        # Building the product url
         prd_url: str = f"{base_url}{prd_str}"
         ins_content: BeautifulSoup = get_html_contents(prd_url)
 
+        # Gathering the web elements that contains the insurance categories
         elements: List[str] = ins_content.find_all('li', attrs={'class': 'selected'})
-        # all_anchors: List[str] = [lst.find_all('a') for lst in element]
 
         category_urls, categories = [], []
         
+        # Looping through the captured web elements to capture the urls for each category
         for element in elements:
             for anchor in element.find_all('a'):
                 category_urls.append(anchor.get('href'))
@@ -34,28 +42,16 @@ def download_insurance_docs(base_url: str, prd_str: str, lic_url: str):
             
         logger.info("Compiling the insurance categories...")
 
-        # navigating to each insurance category 
+        # navigating to each insurance category
         for idx, cat_url in enumerate(category_urls):
-            cat_content: BeautifulSoup = get_html_contents(cat_url)
-            policy_rows: List[str] = cat_content.find_all('td')
+            policy_urls: List[str] = get_policy_urls(lic_url, cat_url) # getting the policy urls for each category
 
-            for row in policy_rows:
-                for policy_link in row.find_all('a'):
-                    policy_url: str = f"{lic_url}{policy_link.get('href')}"
+            # Capturing the documents for each policy for each category
+            for url in policy_urls:
+                links, policy = get_policy_details(url)
+                links = get_relevant_documents(links)
+                save_documents("insurance", lic_url, links, policy, categories[idx]) # Saving documents
 
-                    print(policy_url)
-
-                    policy_details: BeautifulSoup = get_html_contents(policy_url)
-
-                    anchors: List[str] = policy_details.find_all('section', {'id': 'maincontent'})[0].find_all('a')
-                    links: List[str] = [anchor.get('href') for anchor in anchors]
-                    policy: str = policy_details.find_all('section', {'id': 'maincontent'})[0].find('h5').get_text()
-
-                    if len(links) > 3:
-                        links = links[-3:]
-                        
-                    save_documents("insurance", lic_url, 
-                                links, policy, categories[idx])
-
+        logger.info("Insurance documents are downloaded...")
     except Exception as e:
         raise CustomException(e, sys)
